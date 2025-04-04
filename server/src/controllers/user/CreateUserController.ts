3ad82cd56c2af18a37dc543b4f1ffd75c21b7dd3
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-04-03 21:29:10 -0300

    refactor(validation): add validation with ZOD

--- server/src/controllers/user/CreateUserController.ts
+++ server/src/controllers/user/CreateUserController.ts
@@ -1,20 +1,23 @@
-import { Request, Response } from "express";
-import { ApiError } from "../../utils/ApiError";
+import { NextFunction, Request, Response } from 'express';
+import { z } from 'zod';
 
 export class CreateUserController {
-  static handle(req: Request, res: Response) {
-    // AINDA FALTA DESESTRUTURAR ALGUNS DADOS DE USUÁRIO
-    const { email, password } = req.body;
+  static handle(req: Request, res: Response, next: NextFunction) {
+    try {
+      const bodyScheme = z.object({
+        email: z.string({ required_error: 'O e-mail é obrigatório.' }).email(),
+        password: z
+          .string({ required_error: 'A senha é obrigatória.' })
+          .trim()
+          .min(8, { message: 'A senha deve ter pelo menos 8 dígitos' }),
+      });
 
-    // UTILIZAR O ZOD PARA VALIDAÇÃO
-    if (!email) {
-      throw new ApiError('O e-mail é obrigatório.');
+      // AINDA FALTA DESESTRUTURAR ALGUNS DADOS DE USUÁRIO
+      const { email, password } = bodyScheme.parse(req.body);
+
+      res.status(200).json({ ok: true });
+    } catch (error) {
+      next(error);
     }
-
-    if (!password) {
-      throw new ApiError('A senha é obrigatória.');
-    }
-
-    res.status(200).json({ ok: true });
   }
 }
--- server/src/middlewares/errorHandling.ts
+++ server/src/middlewares/errorHandling.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from 'express';
 import { ApiError } from '../utils/ApiError';
+import { ZodError } from 'zod';
 
 export function errorHandling(err: any, req: Request, res: Response, _: any) {
   if (err instanceof ApiError) {
     return void res
       .status(err.statusCode)
       .json({ ok: false, message: err.message });
+  }
+
+  if (err instanceof ZodError) {
+    return void res
+      .status(400)
+      .json({ ok: false, message: 'erro de validação.', issues: err.flatten() });
   }
 
   return void res
