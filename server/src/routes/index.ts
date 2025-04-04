e43b8b32740a8b83c65a2ce940a929be47964f0f
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-04-04 19:36:58 -0300

    docs(comment): add comment

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,7 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
+// Automates route export
 export default fs
   .readdirSync(__dirname)
   .filter((file) => file.indexOf('.') !== 0 && file !== 'index.ts')
