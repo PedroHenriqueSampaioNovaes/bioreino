07dc05e93afa700e2b5d5bfec0c560b00035291b
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-04-07 16:03:48 -0300

    feat(user controller): updates user controller with new validations and executes createUserService

--- server/src/controllers/user/CreateUserController.ts
+++ server/src/controllers/user/CreateUserController.ts
@@ -1,22 +1,31 @@
 import { NextFunction, Request, Response } from 'express';
+
 import { z } from 'zod';
+
 import { CreateUserService } from '../../services/user/CreateUserService';
 
 export class CreateUserController {
-  static handle(req: Request, res: Response, next: NextFunction) {
+  static async handle(req: Request, res: Response, next: NextFunction) {
     try {
       const bodyScheme = z.object({
+        name: z
+          .string({ required_error: 'O nome é obrigatório.' })
+          .min(5, 'O nome deve ter pelo menos 5 dígitos.'),
         email: z.string({ required_error: 'O e-mail é obrigatório.' }).email(),
         password: z
           .string({ required_error: 'A senha é obrigatória.' })
           .trim()
-          .min(8, { message: 'A senha deve ter pelo menos 8 dígitos' }),
+          .min(8, { message: 'A senha deve ter pelo menos 8 dígitos.' }),
+        plan: z.string({
+          required_error: 'O plano de assinatura é obrigatório.',
+        }),
       });
 
-      // AINDA FALTA DESESTRUTURAR ALGUNS DADOS DE USUÁRIO
-      const { email, password } = bodyScheme.parse(req.body);
+      const bodyData = bodyScheme.parse(req.body);
 
-      res.status(200).json({ ok: true });
+      const user = await CreateUserService.execute(bodyData);
+
+      res.status(201).json(user);
     } catch (error) {
       next(error);
     }
