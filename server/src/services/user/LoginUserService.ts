9b39599d4afa409f8ecf36d00560a82d6275d719
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-04-16 21:53:40 -0300

    feat(loginuserservice): add a condition to avoid comparing the password with the one from the db

--- server/src/services/user/LoginUserService.ts
+++ server/src/services/user/LoginUserService.ts
@@ -12,18 +12,19 @@ interface ILoginRequest {
 
 export class LoginUserService {
   static async execute({ email, password }: ILoginRequest) {
-    const user = await User.findOne({ email }).populate(
-      'plan',
-      '-benefits -price'
-    );
+    const user = await User.findOne({ email })
+      .populate('plan', '-benefits -price')
+      .select('+accountExpiresAfter');
     if (!user) {
       throw new ApiError('E-mail ou senha incorreto.', 400);
     }
 
-    // compare password with db password
-    const matchPassword = compareSync(password, user.password);
-    if (!matchPassword) {
-      throw new ApiError('E-mail ou senha incorreto.');
+    // compare the password with db password if it's not a temporary account
+    if (user.accountExpiresAfter === undefined) {
+      const matchPassword = compareSync(password, user.password);
+      if (!matchPassword) {
+        throw new ApiError('E-mail ou senha incorreto.');
+      }
     }
 
     const token = jwt.sign(
