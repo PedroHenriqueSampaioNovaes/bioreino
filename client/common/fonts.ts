4727520bf2ded45bfb619258f1a24edbca88f255
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-04-25 00:36:40 -0300

    feat(nextjs): initialize frontend with the nextjs

---
+++ client/common/fonts.ts
@@ -0,0 +1,11 @@
+import { Fredoka, Catamaran } from 'next/font/google';
+
+export const fredoka = Fredoka({
+  variable: '--font-primary-fredoka',
+  subsets: ['latin'],
+});
+
+export const catamaran = Catamaran({
+  variable: '--font-secondary-catamaran',
+  subsets: ['latin'],
+});
---
+++ client/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next';
+import classNames from 'classnames';
+import { catamaran, fredoka } from '../../common/fonts';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Bioreino',
+  description:
+    'Site de cursos de biologia para estudantes de ensino fundamental e médio, além de profissionais da área.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={classNames(fredoka.variable, catamaran.variable)}>
+        {children}
+      </body>
+    </html>
+  );
+}
