ba19e2eae7034797fc5432f273ba96a1761bb9e0
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-05-06 00:29:37 -0300

    refactor(models): change price type to Number

--- server/src/models/PlanModel.ts
+++ server/src/models/PlanModel.ts
@@ -2,14 +2,14 @@ import { Schema, model } from 'mongoose';
 
 export interface IPlan {
   name: string;
-  price: string;
+  price: number;
   benefits: string[];
   fullaccess?: boolean;
 }
 
 const plan = new Schema<IPlan>({
   name: { type: String, required: true },
-  price: { type: String, required: true },
+  price: { type: Number, required: true },
   benefits: { type: [String], required: true },
   fullaccess: Boolean,
 });
