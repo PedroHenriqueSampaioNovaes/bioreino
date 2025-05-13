227d28b7a49d16ccc10775a186bb242dbea15b51
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-05-13 16:27:13 -0300

    feat(types): add user type

---
+++ client/src/common/@types/user.ts
@@ -0,0 +1,26 @@
+type ILastWatched = {
+  course: {
+    courseTitle: string;
+    slug: string;
+    professor: string;
+    imageUrl: string;
+  };
+  lesson: {
+    lessonTitle: string;
+    lessonDescription: string;
+    slug: string;
+  };
+  watchedAt: string;
+};
+
+export type IUser = {
+  _id: string;
+  name: string;
+  email: string;
+  plan: {
+    _id: string;
+    name: string;
+    fullaccess: boolean;
+  };
+  lastWatched: ILastWatched;
+};
