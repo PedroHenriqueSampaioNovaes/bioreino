4d7f7dda49b543a368bfe35a4957c052fe5bacab
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-05-13 18:45:43 -0300

    feat(layout): add UserContextProvider and fetch action getUser

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,21 +4,27 @@ import { catamaran, fredoka } from '../common/fonts';
 import 'normalize.css';
 import './globals.css';
 
+import { UserContextProvider } from '@/context/UserContext';
+
+import getUser from '@/action/user-get';
+
 export const metadata: Metadata = {
   title: 'Bioreino',
   description:
     'Site de cursos de biologia para estudantes de ensino fundamental e médio, além de profissionais da área.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data: user } = await getUser();
+
   return (
     <html lang="pt-br">
       <body className={classNames(fredoka.variable, catamaran.variable)}>
-        {children}
+        <UserContextProvider userData={user}>{children}</UserContextProvider>
       </body>
     </html>
   );
