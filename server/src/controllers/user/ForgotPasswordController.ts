7c6cfc1668cda458bd51a05d9b59b373bd535451
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-05-14 19:54:05 -0300

    refactor(forgotpassword api): rename file name to ForgotPasswordService and update function name

--- server/src/controllers/user/ForgotPasswordController.ts
+++ server/src/controllers/user/ForgotPasswordController.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 import { z } from 'zod';
 
-import { ForgotPasswordService } from '../../services/user/ForgotPasswordServiceService';
+import { ForgotPasswordService } from '../../services/user/ForgotPasswordService';
 import { sendMail } from '../../modules/sendMail';
 
 export class ForgotPasswordController {
