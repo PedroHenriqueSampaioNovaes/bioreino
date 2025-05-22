f55b3edd6bd135be1d2987e2c7caab6e49a56ef3
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-05-22 20:41:26 -0300

    feat(action): add action course-get

---
+++ client/src/action/course-get.ts
@@ -0,0 +1,24 @@
+'use server';
+
+import { COURSE_GET } from '@/common/api';
+import apiError from '@/common/apiError';
+
+import { ICourse, ICourseGet } from '@/common/@types/course';
+
+export default async function getCourse({
+  free,
+  limit,
+  planId,
+}: ICourseGet = {}) {
+  try {
+    const { url } = COURSE_GET({ free, limit, planId });
+    const response = await fetch(url, { cache: 'force-cache' });
+
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.message);
+
+    return { data: data as ICourse[], error: '', ok: true };
+  } catch (error) {
+    return apiError(error);
+  }
+}
