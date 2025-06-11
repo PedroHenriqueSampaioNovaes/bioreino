09ab4214c62fcb4cc2a73ec667879e485b45c410
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-06-11 20:35:16 -0300

    feat(controllers): add the parameter planId to filter by the user's subscription plan categories

--- server/src/controllers/category/ListCategoryController.ts
+++ server/src/controllers/category/ListCategoryController.ts
@@ -3,9 +3,15 @@ import { NextFunction, Request, Response } from 'express';
 import { ListCategoryService } from '../../services/category/ListCategoryService';
 
 export class ListCategoryController {
-  static async handle(req: Request, res: Response, next: NextFunction) {
+  static async handle(
+    req: Request<any, any, any, { plan_id?: string }>,
+    res: Response,
+    next: NextFunction
+  ) {
     try {
-      const categories = await ListCategoryService.execute();
+      const { plan_id } = req.query;
+
+      const categories = await ListCategoryService.execute({ planId: plan_id });
 
       res.json(categories);
     } catch (error) {
--- server/src/services/category/ListCategoryService.ts
+++ server/src/services/category/ListCategoryService.ts
@@ -1,8 +1,33 @@
-import { Category } from '../../models/Category';
+import { FilterQuery, Types } from 'mongoose';
+
+import { Category, ICategory } from '../../models/Category';
+import { Plan } from '../../models/PlanModel';
+
+import { ApiError } from '../../utils/ApiError';
+
+interface ICourseRequest {
+  planId?: string;
+}
 
 export class ListCategoryService {
-  static async execute() {
-    const categories = await Category.find();
+  static async execute({ planId }: ICourseRequest) {
+    const query = {} as FilterQuery<ICategory>;
+
+    if (planId && Types.ObjectId.isValid(planId)) {
+      const plan = await Plan.findById(planId);
+
+      if (!plan)
+        throw new ApiError('Nenhum plano de assinatura foi encontrado.');
+
+      if (!plan.fullaccess) {
+        query['$or'] = [
+          { plan: null },
+          { plan: new Types.ObjectId(plan.id as string) },
+        ];
+      }
+    }
+
+    const categories = await Category.find(query);
 
     return categories;
   }
