dad17f3cc1c6e9164620e4be1355af25afd9352f
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-06-12 19:15:43 -0300

    feat(@types): add the interface IProgress

--- client/src/common/@types/course.ts
+++ client/src/common/@types/course.ts
@@ -16,3 +16,17 @@ export interface ICourse {
   free: boolean;
   plan: { _id: string; name: string };
 }
+
+export interface IProgress {
+  _id: string;
+  userId: string;
+  courseId: string;
+  completed: boolean;
+  createdAt: string;
+  lastWatchedAt: string;
+  lessons: {
+    lessonId: string;
+    watchedAt: string;
+  }[];
+  progress: number;
+}
