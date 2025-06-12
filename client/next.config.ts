7a84e3bd74d815bf122b26d1552a405eed9943c3
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-06-12 19:45:24 -0300

    build(next.config): add remotePatterns to images

--- client/next.config.ts
+++ client/next.config.ts
@@ -9,6 +9,34 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/photos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        port: '',
+        pathname: '/fotos-gratis/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'marsemfim.com.br',
+        port: '',
+        pathname: '/wp-content/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.mundoeducacao.uol.com.br',
+        port: '',
+        pathname: '/mundoeducacao/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
