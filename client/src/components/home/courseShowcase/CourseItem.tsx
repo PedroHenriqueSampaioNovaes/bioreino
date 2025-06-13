1f5d5b45b513d3a8446919de775ee2a36bd890f9
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-06-13 17:24:07 -0300

    refactor(courseshowcase): refactor background image using now image next component

--- client/src/components/home/courseShowcase/CourseItem.tsx
+++ client/src/components/home/courseShowcase/CourseItem.tsx
@@ -2,6 +2,7 @@
 
 import styles from './courseItem.module.css';
 import classNames from 'classnames';
+import Image from 'next/image';
 
 import { ICourse } from '@/common/@types/course';
 
@@ -16,15 +17,19 @@ export default function CourseItem({ course }: ICourseItemProps) {
         <span className={classNames(styles.subscription)}>
           {course.plan.name}
         </span>
-        <div
-          className={classNames(styles.background)}
-          style={{ backgroundImage: `url(${course.imageUrl})` }}
-        ></div>
+        <Image
+          src={course.imageUrl}
+          alt=""
+          width={966}
+          height={750}
+          priority
+          sizes="(max-width: 1023px) 100vw, 25vw"
+        />
         <div className={classNames(styles.details)}>
           <h3 className={classNames(styles.courseName)}>{course.title}</h3>
           <span className={classNames(styles.teacher)}>
             Prof. {course.professor}
-          </span> 
+          </span>
         </div>
       </a>
     </li>
