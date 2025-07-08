c663bef04bb24ffef430e07282c73f88e3fd846f
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-07-07 21:00:01 -0300

    feat(logout): delete user_id in cookies

--- client/src/action/logout.ts
+++ client/src/action/logout.ts
@@ -5,5 +5,6 @@ import { redirect } from 'next/navigation';
 
 export default async function logout() {
   (await cookies()).delete('token');
+  (await cookies()).delete('user_id');
   redirect('/login');
 }
