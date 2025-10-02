dab5c581a0df0ccbc74d49c3ac11b195bb4ed490
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-10-02 01:45:26 -0300

    feat(routes): testing only courseRoutes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 import fs from 'node:fs';
 import path from 'node:path';
+import courseRoutes from './courseRoutes';
 
 type Route = {
   router: Router;
   baseRoute: string;
 };
 
+export default [courseRoutes];
+
 // Automates route export
-export default fs
-  .readdirSync(__dirname)
-  .filter((file) => file.indexOf('.') !== 0 && file !== 'index.ts')
-  .map((file) => require(path.resolve(__dirname, file)).default) as Route[];
+// export default fs
+//   .readdirSync(__dirname)
+//   .filter((file) => file.indexOf('.') !== 0 && file !== 'index.ts')
+//   .map((file) => require(path.resolve(__dirname, file)).default) as Route[];
