838d1d1cb3c5e382cc9858f40cc7ff2925f0295d
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-10-02 01:48:35 -0300

    feat(routes): testing new routes on vercel to find any bug

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,13 +2,16 @@ import { Router } from 'express';
 import fs from 'node:fs';
 import path from 'node:path';
 import courseRoutes from './courseRoutes';
+import categoryRoutes from './categoryRoutes';
+import coursesProgressRoutes from './coursesProgressRoutes';
+import planRoutes from './planRoutes';
 
 type Route = {
   router: Router;
   baseRoute: string;
 };
 
-export default [courseRoutes];
+export default [courseRoutes, coursesProgressRoutes, categoryRoutes, planRoutes];
 
 // Automates route export
 // export default fs
