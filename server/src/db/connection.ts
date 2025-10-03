b692dea713095deeccce7d6dfc082acc7455b596
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-10-03 16:49:49 -0300

    chore(console.log): remove console.log

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -5,7 +5,6 @@ async function initializeDb() {
     await mongoose.connect(
       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@bioreino.l8j1rrn.mongodb.net/${process.env.DB}`
     );
-    console.log('Conectou-se ao mongodb');
   } catch (error) {
     console.error('Um erro ocorreu ao tentar se conectar com mongoose', error);
   }
