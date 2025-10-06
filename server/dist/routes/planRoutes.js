117d0f81c7d08c4ee943f90ac6aa533d09ec8639
Pedro Henrique Sampaio de Novaes <92189897+PedroHenriqueSampaioNovaes@users.noreply.github.com>
2025-10-06 15:31:27 -0300

    Delete server/dist directory

--- server/dist/routes/planRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const ListSubscriptionController_1 = require("../controllers/subscription/ListSubscriptionController");
-const router = (0, express_1.Router)();
-router.get('/', ListSubscriptionController_1.ListSubscriptionController.handle);
-exports.default = { router, baseRoute: '/subscriptions' };
