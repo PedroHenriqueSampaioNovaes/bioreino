eca0a6c5bc5af52a65f3a06c1f15917ae4e08259
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2025-10-10 02:22:37 -0300

    test

--- server/src/server.ts
+++ server/src/server.ts
@@ -36,7 +36,7 @@ routes.forEach((route) => {
 // Handle error api
 app.use(errorHandling);
 
-console.log(fs.readdirSync(__dirname + '/resources'));
+console.log('tesssste', fs.readdirSync(__dirname + '/resources'));
 
 app.listen(PORT, () =>
   console.log(`Servidor rodando na porta http://localhost:${PORT}`)
