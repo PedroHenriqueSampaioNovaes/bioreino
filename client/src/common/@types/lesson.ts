c85a42296c21733fa79764a8a4684506bb60367b
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-03-22 02:30:21 -0300

    refactor(@types): separate the video and transcription options from ILesson to interface ILessonVideoData

--- client/src/common/@types/lesson.ts
+++ client/src/common/@types/lesson.ts
@@ -1,14 +1,21 @@
 export interface ILesson {
   _id: string;
   description: string;
-  video: string;
-  transcription: string;
   title: string;
   courseId: string;
   slug: string;
   free: boolean;
 }
 
+export interface ILessonVideoData {
+  video: string;
+  transcription: string;
+}
+
 export interface IListLessonGet {
   course_id?: string;
+}
+
+export interface ILessonGet {
+  slug: string;
 }
