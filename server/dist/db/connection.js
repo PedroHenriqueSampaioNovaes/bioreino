7051fba3d3816fa426d0995c287f7d504f5a0ade
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-03-23 02:08:23 -0300

    build(backend): create new build

--- server/dist/db/connection.js
+++ server/dist/db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 async function initializeDb(retries = 5) {
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@bioreino.l8j1rrnu.mongodb.net/${process.env.DB}`);
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@bioreino.l8j1rrn.mongodb.net/${process.env.DB}`);
         console.log('Conectou-se ao MongoDB');
     }
     catch (error) {
