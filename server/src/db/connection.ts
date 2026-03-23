db311e779b471ac3b9dac4bc51c684c1a79f79cb
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-03-23 01:58:21 -0300

    feat(db): adds attempts to connect to the database

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,13 +1,17 @@
 import mongoose from 'mongoose';
 
-async function initializeDb() {
+async function initializeDb(retries: number = 5) {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@bioreino.l8j1rrn.mongodb.net/${process.env.DB}`
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@bioreino.l8j1rrnu.mongodb.net/${process.env.DB}`,
     );
-    console.log('Conectou-se ao mongodb');
+    console.log('Conectou-se ao MongoDB');
   } catch (error) {
-    console.error('Um erro ocorreu ao tentar se conectar com mongoose', error);
+    if (retries > 0) {
+      setTimeout(() => initializeDb(retries - 1), 1000);
+      return;
+    }
+    console.error('Um erro ocorreu ao tentar se conectar com o MongoDB', error);
   }
 }
 initializeDb();
