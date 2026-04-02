059aabfd14df256c9e14a9a1118775af1a534ae4
Pedro Henrique <pedrohenriquesampaiodenovaes@gmail.com>
2026-04-01 21:50:49 -0300

    refactor(footer): obtain the dynamic year

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -3,10 +3,12 @@ import Link from 'next/link';
 import styles from './footer.module.css';
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className={styles.footer}>
       <p>
-        Bioreino © 2025 -{' '}
+        Bioreino © {currentYear} -{' '}
         <Link href="https://github.com/AraraDevs" target="_blank">
           AraraDevs
         </Link>{' '}
